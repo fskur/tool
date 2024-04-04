3d5494e5da11fc68c8068d70a2baf59b25557712
kuriyama <kuriyama@fsfield.co.jp>
2024-04-04 12:38:57 +0900

    [Add] generator

---
+++ generator/app.py
@@ -0,0 +1,47 @@
+import datetime
+import json
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+def generate_calendar_data(selected_year, selected_month):
+    starting_day = datetime.datetime(selected_year, selected_month, 1).weekday()
+    days_in_month = (datetime.datetime(selected_year, selected_month % 12 + 1, 1) - datetime.datetime(selected_year, selected_month, 1)).days
+
+    calendar_data = []
+
+    for day in range(1, days_in_month + 1):
+        day_data = {
+          "month": selected_month,
+          "class": [
+            { "day": day,
+              "time_am": request.json[f"day_{day}"]["time_am"],  # 初期値を空に設定
+              "time_pm": "",  # 初期値を空に設定
+              "menu_am": "",  # 初期値を空に設定
+              "menu_pm": ""   # 初期値を空に設定
+            }
+          ]
+        }
+        calendar_data.append(day_data)
+
+    return calendar_data
+
+@app.route('/run_script', methods=['POST'])
+def run_script():
+    selected_year = int(request.json['year'])
+    selected_month = int(request.json['month'])
+
+    calendar_data = generate_calendar_data(selected_year, selected_month)
+
+    with open("data.json", "w") as file:
+        json.dump(calendar_data, file, indent=2, ensure_ascii=False)
+
+    print("JSONデータをファイルに書き込みました。")
+    return jsonify(calendar_data)
+
+@app.route('/tool/generator/cooking.html')
+def cooking_page():
+    return render_template('cooking.html')
+
+if __name__ == '__main__':
+    app.run(port=8000)
---
+++ generator/rendering.py
@@ -0,0 +1,15 @@
+from jinja2 import Template, Environment, FileSystemLoader
+import json
+
+#テンプレート読み込み
+env = Environment(loader=FileSystemLoader('./', encoding='utf8'))
+tmpl = env.get_template('calendar.j2')
+
+#設定ファイル読み込み
+with open('data.json', 'r', encoding='utf-8') as f:
+    params = json.load(f)
+
+#レンダリングしてhtml出力
+rendered_html = tmpl.render(params)
+with open('result.html', 'w') as f:
+    f.write(rendered_html)
