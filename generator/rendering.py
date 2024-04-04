ad5c8d9d68c79e0473f77a55f980dec6704500aa
kuriyama <kuriyama@fsfield.co.jp>
2024-04-04 17:42:09 +0900

    また入力した値が取れなくなりました

--- generator/rendering.py
+++ generator/rendering.py
@@ -1,18 +1,15 @@
-from jinja2 import Template, Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader
 import json
 
-#テンプレート読み込み
+# テンプレート読み込み
 env = Environment(loader=FileSystemLoader('./', encoding='utf8'))
 tmpl = env.get_template('calendar.j2')
 
-#設定ファイル読み込み
+# 設定ファイル読み込み
 with open('data.json', 'r', encoding='utf-8') as f:
-    params = json.load(f)
+    class_data = json.load(f)
 
-# JSONデータを出力して確認
-print(params)
-
-#レンダリングしてhtml出力
-rendered_html = tmpl.render(params)
+# レンダリングしてhtml出力
+rendered_html = tmpl.render(class_info=class_data[0])
 with open('result.html', 'w') as f:
     f.write(rendered_html)
